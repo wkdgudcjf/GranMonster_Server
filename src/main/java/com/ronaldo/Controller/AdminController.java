0ed8ffc70678446a8a788c34c459e940a843b42b
wkdgudcjf1 <wkdgudcjf1@naver.com>
2018-01-28 21:23:01 +0900

    fdsfas

--- src/main/java/com/ronaldo/Controller/AdminController.java
+++ src/main/java/com/ronaldo/Controller/AdminController.java
@@ -253,10 +253,10 @@ public class AdminController
 	}
 	@RequestMapping(value = "/downloadSDK", method = RequestMethod.POST)
     public View downLoadFile() {
-        String fullPath = context.getRealPath("sdk/Granmonster_SDK_v1.2.unitypackage");
+        String fullPath = context.getRealPath("sdk/Granmonster_SDK_v1.3.unitypackage");
         File downloadFile = new File(fullPath);
         DownloadFileView downloadFileView = new DownloadFileView();
-        downloadFileView.downloadFileName = "Granmonster_SDK_v1.2.unitypackage";
+        downloadFileView.downloadFileName = "Granmonster_SDK_v1.3.unitypackage";
         downloadFileView.file = downloadFile;
         return downloadFileView;
     }
